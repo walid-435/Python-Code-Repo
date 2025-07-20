cb51d419688ed608f15c546882bcaf9c0ef311ab
Walid Rahman <rahman241-35-435@diu.edu.bd>
2025-07-20 22:41:48 +0600

    committing my python files

--- PythonPracticeStage/day9.py
+++ PythonPracticeStage/day9.py
@@ -7,6 +7,16 @@
 f = open("D:\CodeDumpPython\PythonPracticeStage\demo.txt", "r")
 
 #a = f.read - read and return the data of the file
-data = f.read()
+data = f.read(5) #to read the first characters
+line1 = f.readline() #to read a whole line
+print(line1)
 print(data)
-f.close()
+f.close() 
+
+#'r' = open for reading
+#'w' = open for writting, truncating the file first
+#'x' = create a new file and open it for writting
+#'a' = open for writting, appending to the end of the file if it exists
+#'b' = binary mode
+#'t' = text mode
+#'+' = open a disk file for updating
