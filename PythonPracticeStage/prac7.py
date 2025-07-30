4ff984e3bb5fb490c7aef033fe832a5a25f49b81
Walid Rahman <rahman241-35-435@diu.edu.bd>
2025-07-30 09:29:57 +0600

    committing my python files

--- PythonPracticeStage/prac7.py
+++ PythonPracticeStage/prac7.py
@@ -19,3 +19,44 @@ with open("practicefile.txt", "r") as f:
         print("found")
     else:
         print("not found")
+        
+#Q4 - find the line in which the word "learning" occur first
+def check_for_line():
+    word = "learning"
+    data = True
+    line_no = 1
+    with open("practicefile.txt", "r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+                return
+            line_no+=1
+        
+check_for_line()
+
+#Q5 - file containg numbers separated by comma, print the count of even numbers
+with open ("practicefile.txt", "r") as f:
+    data = f.read()
+    num = ""
+    for i in range(len(data)):
+        if(data[i] == ","):
+            print(int(num))
+            num = ""
+        else:
+            num += data[i]
+    
+    if(data[i] % 2 == 0):
+        print()
+        
+#OR
+
+count = 0
+with open("practicefile.txt") as f:
+    data = f.read()
+    nums = data.split(",")
+    for val in nums:
+        if(int(val) % 2 == 0):
+            count +=1
+
+print(count)
